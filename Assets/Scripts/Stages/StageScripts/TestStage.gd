069ec257593975c782ea73cf79036af07e6ebc90
Kamran Haq <KamranHaq@protonmail.com>
2022-08-25 13:52:25 -0700

    Fixed a few small issues

--- Assets/Scripts/Stages/StageScripts/TestStage.gd
+++ Assets/Scripts/Stages/StageScripts/TestStage.gd
@@ -8,11 +8,16 @@ Test stage is a simple demonstration of the current state of visual novel scene 
 #First we preload our actors.
 var TestActor = preload("res://Assets/Scenes/Stages/Actors/TestActor.tscn")
 
+signal stage_loaded
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	#Now we instance our actors.
 	TestActor = TestActor.instance()
+	
+	#Now add them to scene tree
 	Actors.add_child(TestActor)
 	
 	#Load the setting/environment.
@@ -23,8 +28,14 @@ func _ready():
 	yield(TestActor, "finished_action") #Yield to wait until moving is done.
 	TestActor.flip_horizontal()
 	
-	#Now we start scripting!
-	TestActor.start_dialog("This is a test scene. It exists to demonstrate an example stage where actors will talk. It will likely be used for cutscene elements.")
+	#Emit a signal so other nodes know we are ready
+	emit_signal("stage_loaded")
+	
+	#Now we start the scene!
+	TestActor.start_dialog("Hello. I am an actor! I can talk and keep on talking. Wow, isn't talking just so... I don't know? Something? Who knows. Life is weird. ")
 	yield(TestActor, "finished_action")
+	TestActor.start_dialog("This is just meant as a quick demo of this visual novel system. Ideally we will add more functionality later")
+	yield(TestActor, "finished_action")
+	
 	
 
