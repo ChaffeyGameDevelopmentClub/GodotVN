06eca087b53761145062c9cd15cf4f84635a8874
Kamran Haq <KamranHaq@protonmail.com>
2022-09-29 15:27:45 -0700

    Update for main

--- Assets/Scripts/Stages/Events/TimedActorEvent.gd
+++ Assets/Scripts/Stages/Events/TimedActorEvent.gd
@@ -18,7 +18,7 @@ func _init(stage, actor, position, time):
 func start_event():
 	_actor = _actor.instance() # instantiates the preload actor argument
 	_stage.get_node("Actors").add_child(_actor) # adds the image actor to the scene tree
-	_actor.set_stage_position(_position) # sets image actor position
+	_actor.event_set_stage_position(_position) # sets image actor position
 	
 	yield(_stage.get_tree().create_timer(_time), "timeout") # timer before image actor dissapears
 	_actor.queue_free()
